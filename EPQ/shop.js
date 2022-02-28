8ce47d7686ecadb91eefb65ca1e98f0103df371b
Joshua Bunday <93491336+bunday1@users.noreply.github.com>
2022-02-28 09:37:38 +0000

    Add files via upload

--- EPQ/shop.js
+++ EPQ/shop.js
@@ -18,9 +18,27 @@ function func(select){
         var val = select.options[select.selectedIndex].value
         console.log(val)
         var property = select.name
-        console.log(property)
+        // console.log(property)
         if(property == "backgroundColor"){
-            document.body.style.backgroundColor = val
+            // document.body.style.backgroundColor = val
+
+            if (val == "pink"){
+                document.body.style.backgroundImage = "url('img/pinkBG.jpg')"
+                document.body.style.backgroundColor = "rgb(248, 165, 237)"
+            } else if (val == "blue"){
+                document.body.style.backgroundImage = "url('img/blueBG.jpg')"
+                document.body.style.backgroundColor = "rgb(95, 149, 156)"
+            } else if (val == "green") {
+                document.body.style.backgroundImage = "url('img/greenBG.jpg')"
+                document.body.style.backgroundColor = "rgb(90, 199, 144)"
+            } else if (val == "red") {
+                document.body.style.backgroundImage = "url('img/redBG.jpg')"
+                document.body.style.backgroundColor = "rgba(180, 3, 3, 0.747)"
+            } else if (val == "burlywood") {
+                document.body.style.backgroundImage = "url('img/desert.jpg')"
+                document.body.style.backgroundColor = "burlywood"
+            }
+
         } else if (property == "color"){
             document.body.style.color = val
         }
